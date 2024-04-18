fe309d0931fe25a31e3554eef3ef2e143c6345b0
MirjamLiodden <151381038+MirjamLiodden@users.noreply.github.com>
2024-04-18 15:30:45 +0200

    Added parameters

---
+++ Crank–Nicolson.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parametere
+L = 1.0  # Lengde av domenet
+T = 1.0  # Total tid
+Nx = 100  # Antall romsteg
+Nt = 1000  # Antall tidssteg
+dx = L / Nx  # Romsteg
+dt = T / Nt  # Tidssteg
